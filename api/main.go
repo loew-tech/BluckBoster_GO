9eefdecd66729a8f1f8754218657ed5313d8a8a3
loew-tech <loew.technology@gmail.com>
2024-08-27 16:46:27 -0600

    adding cors permissions

--- api/main.go
+++ api/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"blockbuster/api/endpoints"
 	"fmt"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
-
 
 func main() {
 	fmt.Println("hello world")
 	router := gin.Default()
-	router.GET("/movies", endpoints.GetMovies)
+
+	router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"*"},
+        AllowMethods:     []string{"*"},
+        AllowHeaders:     []string{"Origin"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        AllowOriginFunc: func(origin string) bool {
+            return origin == "localhost:8080"
+        },
+        MaxAge: 12 * time.Hour,
+    }))
+
+	router.GET("/api/v1/movies", endpoints.GetMovies)
 	
 	
 	router.Run("localhost:8080")
