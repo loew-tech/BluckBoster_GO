8335ba85756808ff8c36ad63da54be54b1b8853d
loew-tech <loew.technology@gmail.com>
2024-09-10 17:00:04 -0600

    orginizational refacotrs to db builders

--- DB_BUILDERS/build_dbs.py
+++ DB_BUILDERS/build_dbs.py
@@ -5,17 +5,17 @@ import boto3
 
 
 if __name__ == '__main__':
-    os.system('cd ./movies & scrapy crawl movies')
-    os.system('cd ./simpsons & scrapy crawl simpsons')
 
     dynamodb = boto3.resource('dynamodb')
-
+    
+    os.system('cd ./movies & scrapy crawl movies')
     with open('movies.json', encoding='utf-8') as movies_json:
         movies, table = json.load(movies_json), dynamodb.Table('BluckBoster_movies')
         with table.batch_writer() as batch:
             for movie in movies:
                 batch.put_item(Item=movie)
 
+    os.system('cd ./simpsons & scrapy crawl simpsons')
     with open('simpsons.json', encoding='utf-8') as character_json:
         characters, table = json.load(character_json), dynamodb.Table('BluckBoster_members')
         with table.batch_writer() as batch:
