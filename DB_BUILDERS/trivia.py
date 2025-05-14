230491ce864d4baf2922f683c5317643cd69ac9b
loew-tech <loew.technology@gmail.com>
2025-05-14 11:39:53 -0600

    script for generating trivia and adding to movie table

--- DB_BUILDERS/trivia.py
+++ DB_BUILDERS/trivia.py
@@ -1,6 +1,9 @@
 import json
+import re
+from time import sleep
 from typing import Generator
 
+import boto3
 from google import genai
 from google.genai.types import GenerateContentResponse
 
@@ -10,23 +13,25 @@ with open('.env') as env_:
 client = genai.Client(api_key=API_KEY) 
 MODEL = "gemini-2.0-flash"
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('BluckBoster_movies')
 
-def get_movie_names() -> Generator:
+
+def get_movies() -> Generator:
     with open('movies.json', encoding='utf-8') as json_:
         data = json.load(json_)
 
     for d in data:
-        yield (d['title'], d['year'])
+        yield (d['title'], d['year'], d['id'])
 
 
 def get_trivia(movie: str, year: int | str) -> str:
     def parse_response(resp: GenerateContentResponse) -> str:
         trivia = []
-        text = resp.text.split('**Question ')
-        for t in text[1:]:
-            print(f'\t{t=}')
-            _, q, *_, ans = t.strip().split('\n\n')
-            ans = ans.split('\n')[0]
+        pattern = r'\\*\\*Question \d+\\*\\*'
+        trivia_qs = re.split(pattern, resp.text)[1:]
+        for t in trivia_qs:
+            q, ans = t.replace('*', '').strip().replace('\n', '').split('Answer:')
             trivia.append(f'{q}:{ans}')
         return '&:&'.join(trivia)
 
@@ -38,12 +43,28 @@ def get_trivia(movie: str, year: int | str) -> str:
     return parse_response(response)
 
 
+def update_movie(id_, trivia: str) -> None:
+    if not trivia:
+        return
+
+    key = {'id': id_}
+    expr_attrs_vals = {':t': trivia}
+    update_expr = 'SET trivia = :t'
+    
+    table.update_item(
+        Key=key,
+        ReturnValues='NONE',
+        UpdateExpression=update_expr,
+        ExpressionAttributeValues=expr_attrs_vals,
+    )
+    
+
+
 if __name__ == '__main__':
     print('Hello trivia')
-    for entry in get_movie_names():
-        print(entry)
-        m, y = entry
+    for i, entry in enumerate(get_movies()):
+        m, y, movie_id = entry
         triv = get_trivia(m, y)
-        print(f'\n{triv=}')
-        break
+        update_movie(movie_id, triv)
+        sleep(3)
     print('All Done :)')
