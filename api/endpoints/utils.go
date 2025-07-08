62f2a8e736cd48a251077279f70b0454acfdb943
loew-tech <147945762+loew-tech@users.noreply.github.com>
2025-07-08 08:37:01 -0600

    Api choice in database (#20)
    * rest endpoint for setting member api choice
    
    * gql mutation for set api choice
    
    * fixing string format
    
    * making dyanmodbClient a singleton

--- api/endpoints/utils.go
+++ api/endpoints/utils.go
@@ -3,15 +3,25 @@ package endpoints
 import (
 	"context"
 	"log"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
+var (
+	dynamoClient  *dynamodb.Client
+	once          sync.Once
+)
+
 func GetDynamoClient() *dynamodb.Client {
-	config, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalln("FAILED TO INSTANTIATE MemberRepo", err)
-	}
-	return dynamodb.NewFromConfig(config)
+	once.Do(func() {
+		ctx := context.Background()
+		cfg, err := config.LoadDefaultConfig(ctx)
+		if err != nil {
+			log.Fatalln("FAILED TO INSTANTIATE MemberRepo", err)
+		}
+		dynamoClient = dynamodb.NewFromConfig(cfg)
+	})
+	return dynamoClient
 }
